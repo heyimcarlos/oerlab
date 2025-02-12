7b73d390b32f0e2e2196463fdda031f741958f7d
Carlos De la Cruz <carlosdlc.dev@gmail.com>
2025-02-12 00:23:22 -0500

    cleanup

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,12 +9,12 @@ export function Footer() {
         <div className="flex items-center gap-3">
           <CommandIcon className="sm:block hidden w-5 h-5 text-muted-foreground" />
           <p className="text-center">
-            Build by{' '}
-            <Link className="px-1 underline underline-offset-2" href="https://github.com/heyimcarlos">
-              Carlos (for now)
-            </Link>
-            . The source code is available on{' '}
-            <Link className="px-1 underline underline-offset-2" href="https://github.com/CentennialOERLab/student-site">
+            Build by&nbsp;
+            <Link className="underline underline-offset-2" href="https://github.com/heyimcarlos">
+              Carlos
+            </Link>{' '}
+            (for now). The source code is available on&nbsp;
+            <Link className="underline underline-offset-2" href="https://github.com/CentennialOERLab/student-site">
               GitHub
             </Link>
             .
