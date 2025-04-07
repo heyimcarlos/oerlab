f1b3b23652af898d5b39d7476d5243fbdb8cb859
Carlos De la Cruz <carlosdlc.dev@gmail.com>
2025-04-07 14:22:03 -0400

    add extra information abou lab members

--- lib/lab-members.ts
+++ lib/lab-members.ts
@@ -1,23 +1,27 @@
 export const labMembers: LabMember[] = [
   {
     name: 'Carlos De La Cruz',
-    title: 'Lead Developer',
-    bio: 'Software Engineering Technology - AI',
+    role: 'Lead Developer & Coordinator',
+    bio: 'Ex competitive gamer turned software engineer. I utilize web technologies to solve complex problems and enjoy activities like snowboarding and BJJ.',
+    program: 'Software Engineering Technology - AI',
     image: '/team/carlosdelacruz.png',
-    linkedIn: 'https://www.linkedin.com/in/carlos-dev/',
     website: 'https://heyimcarlos.dev',
+    linkedIn: 'https://www.linkedin.com/in/carlos-dev/',
+    github: 'https://github.com/heyimcarlos',
   },
   {
     name: 'Gichul Jaun',
-    title: 'Student Developer',
+    role: 'Student Developer',
     bio: 'Software Engineering Technology - AI',
+    program: 'Software Engineering Technology - AI',
     image: '/team/gichuljaun.jpg',
     linkedIn: 'https://www.linkedin.com/in/gichuljaun/',
     website: 'https://gichul-j.com/',
   },
   {
     name: 'Leslie Cui',
-    title: 'Student Developer',
+    role: 'Student Developer',
+    program: 'Software Engineering Technology',
     bio: 'Software Engineering Technology',
     image: '/team/leslie.JPG',
     linkedIn: 'www.linkedin.com/in/leslie-lc',
@@ -25,8 +29,9 @@ export const labMembers: LabMember[] = [
   },
   {
     name: 'Naomi Lopez Mendoza',
-    title: 'Student Developer',
+    role: 'Student Developer',
     bio: 'Game Programming',
+    program: 'Game Programming',
     image: '/team/naomi.jpg',
     linkedIn: 'https://www.linkedin.com/in/nmendo16/',
     website: 'https://naomilopez19.wixsite.com/naomisportfolio',
@@ -35,8 +40,9 @@ export const labMembers: LabMember[] = [
 
 export type LabMember = {
   name: string
-  title: string
   bio: string
+  program: string
+  role: string
   image: string
   website?: string
   linkedIn?: string
