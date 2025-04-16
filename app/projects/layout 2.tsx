f30acefc1dd86fa494400b7fd48294f2ed3b84dd
Carlos De la Cruz <carlosdlc.dev@gmail.com>
2025-04-16 12:05:32 -0400

    remove duplicated pages

--- app/projects/layout 2.tsx
+++
@@ -1,5 +0,0 @@
-import { type PropsWithChildren } from 'react'
-
-export default function ProjectsLayout({ children }: PropsWithChildren) {
-  return <div className="flex flex-col items-start justify-center pt-8 pb-10 w-full mx-auto">{children}</div>
-}
