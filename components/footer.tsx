898fb22d536838ea365a2c7db18407513e451ee9
Carlos De la Cruz <carlosdlc.dev@gmail.com>
2025-04-23 16:24:58 -0400

    quickfix footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ import { Icons } from './icons'
 export function Footer() {
   return (
     <footer className="border-t w-full p-5">
-      <div className="container flex items-center sm:justify-between justify-center sm:gap-0 gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3 max-sm:px-4">
+      <div className="container flex items-center flex-col-reverse md:flex-row sm:justify-between justify-center  gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3 max-sm:px-4">
         {/* <Image src="/logo.png" alt={'logo'} width={300} height={300} quality={90} className="object-cover h-24 w-24" /> */}
         <div className="flex gap-3 max-w-96 flex-col">
           <p className="">
@@ -27,7 +27,7 @@ export function Footer() {
             <Icons.sa className="h-8 w-8" />
           </div>
         </div>
-        <div className="gap-4 items-center hidden md:flex">
+        <div className="gap-4 flex md:grid md:grid-cols-2">
           <FooterButtons />
         </div>
       </div>
@@ -37,7 +37,7 @@ export function Footer() {
 
 export function FooterButtons() {
   return (
-    <div className="grid grid-cols-2 gap-4">
+    <>
       <Link href="/about" className={buttonVariants({ variant: 'outline', size: 'sm' })}>
         <HeartIcon className="h-4 w-4 mr-2 text-red-600 fill-current" />
         About
@@ -57,6 +57,6 @@ export function FooterButtons() {
         <Icons.gitHub className="h-[0.8rem] w-4 mr-2 text-primary fill-current" />
         Source
       </Link>
-    </div>
+    </>
   )
 }
